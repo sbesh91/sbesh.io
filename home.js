a116559c9d4b0d117dc34940d8a6e5bd0a457489
sbesh91 <s.beshensky@gmail.com>
2016-09-27 00:11:11 -0500

    animation testing

--- home.js
+++ home.js
@@ -7,10 +7,15 @@ var redis = require("redis"),
   handlebars = require("express-handlebars"),
   path = require("path");
 
-client.set("string key", "redis working", redis.print);
+  client.set("string key", "redis working", redis.print);
+
+app.use('/static', express.static(path.join(__dirname, "/styles")));
+app.use('/static', express.static(path.join(__dirname, "/scripts")));
+app.use('/static', express.static(path.join(__dirname, "/node_modules/vue/dist")));
+app.use('/static', express.static(path.join(__dirname, "/node_modules/web-animations-js")));
 
 app.set('views', path.join(__dirname, "views"));
-app.engine("handlebars", handlebars({defaultLayout: path.join(__dirname, "/views/layouts/_layout")}));
+app.engine("handlebars", handlebars({defaultLayout: path.join(__dirname, "_layout")}));
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
