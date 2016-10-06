194df144b4504f5e451d05d2d6615c6f816b8922
sbesh91 <s.beshensky@gmail.com>
2016-10-05 20:50:16 -0500

    basic server side routing passthrough. next step should be http/2

--- home.js
+++ home.js
@@ -1,31 +1,23 @@
 /*jslint node: true */
 "use strict";
-var redis = require("redis"),
-  client = redis.createClient(),
+var
+  fs = require('fs'),
   express = require('express'),
   app = express(),
-  handlebars = require("express-handlebars"),
   path = require("path");
 
-  client.set("string key", "redis working", redis.print);
+var srcPath = path.join(__dirname, "/build/unbundled/");
+var directories = fs.readdirSync(srcPath).filter(function(file){
+  return fs.statSync(path.join(srcPath, file)).isDirectory();
+});
 
-app.use('/static', express.static(path.join(__dirname, "/styles")));
-app.use('/static', express.static(path.join(__dirname, "/scripts")));
-app.use('/static', express.static(path.join(__dirname, "/node_modules/vue/dist")));
-app.use('/static', express.static(path.join(__dirname, "/node_modules/web-animations-js")));
+app.use('/', express.static(srcPath));
+for(var i of directories){
+  app.use('/', express.static(path.join(srcPath, i)));
+}
 
-app.set('views', path.join(__dirname, "views"));
-app.engine("handlebars", handlebars({defaultLayout: path.join(__dirname, "_layout")}));
-app.set('view engine', 'handlebars');
-
-app.get('/', function (req, res) {
-  client.get("string key",function(err, value){
-    if (err) {
-      res.send(err);
-    } else {
-      res.render("index",{data: value});
-    }
-  });
+app.get('/*', function (req, res) {
+  res.sendFile(srcPath + 'index.html');
 });
 
 app.listen(8080, function () {
